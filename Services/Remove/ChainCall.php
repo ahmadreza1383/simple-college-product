96416972bb411b6b936c4f22bc803fb21eba3075
ahmadreza1383 <ahmadrezabashari@gmail.com>
2023-05-22 15:59:50 +0330

    Added method judge for ChainCall class in RemoveService

--- Services/Remove/ChainCall.php
+++ Services/Remove/ChainCall.php
@@ -5,15 +5,16 @@ class ChainCall
 {
     public static function __callStatic($method, $args)
     {
-        (new static)->$method(...$args);
-
-        return new static;
+        return self::judge((new static)->$method(...$args), new static);
     }
 
     public function __call($method, $args)
     {
-        $method = $this->$method(...$args);
+        return self::judge($this->$method(...$args), $this);
+    }
 
-        return (is_null($method)) ? $this : $method;
+    private static function judge($method, $callback)
+    {
+        return (is_null($method)) ? $callback : $method;
     }
 }
